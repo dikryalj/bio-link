9e11a8b68b6e9bdc0a3698fd58f40b653cd85569
dikryalj <146651969+dikryalj@users.noreply.github.com>
2024-02-09 20:00:08 +0700

    Update int.js

--- int.js
+++ int.js
@@ -8,7 +8,7 @@ closeIcon = toast.querySelector(".close-icon");
 window.onload = ()=>{
     function ajax(){
         let xhr = new XMLHttpRequest(); 
-        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);=
+        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
         xhr.onload = ()=>{
             if(xhr.status == 200 && xhr.status < 300){
                 toast.classList.remove("offline");
